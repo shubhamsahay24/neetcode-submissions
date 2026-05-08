9802a3ec0fb17b91047c69315f66f9ac1409d80d
shubhamsahay24 <95434283+shubhamsahay24@users.noreply.github.com>
2026-05-09 02:02:52 +0530

    Add: kth-smallest-integer-in-bst - submission-0

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-0.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        
+        Stack<TreeNode> st = new Stack<>();
+        int count=0;
+        while(root!=null || !st.isEmpty()) {
+            if(root!=null) {
+                st.push(root);
+                root = root.left;
+            } else {
+                TreeNode temp = st.pop();
+                count++;
+                if(count==k)
+                    return temp.val;
+                root = temp.right;
+            }
+        }
+        return 0;
+    }
+
+    private void inOrder(TreeNode root, List<Integer> list) {
+        if(root==null) {
+            return;
+        }
+
+        inOrder(root.left, list);
+        list.add(root.val);
+        inOrder(root.right, list);
+    }
+}
